b72c1d8ea9c085aca0aa13cc9ec99e4215180dd2
Alexandr <pushkarouv98@mail.ru>
2022-02-02 16:45:36 +1000

    Init project

---
+++ cmake-build-debug/CMakeFiles/MatrixCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixCpp.dir/main.cpp.obj"
+  "MatrixCpp.exe"
+  "MatrixCpp.exe.manifest"
+  "MatrixCpp.pdb"
+  "libMatrixCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
